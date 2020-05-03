877506a3204e739709b6cd04be42127810d30dde
Gaurav V <gv71.msrit@gmail.com>
2020-05-03 19:09:28 +0530

    removed boilerplate files

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  /*
   {
     path: "/",
     name: "Home",
@@ -17,8 +17,8 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import( "../views/About.vue")
+  }*/ 
 ];
 
 const router = new VueRouter({
---
+++ src/views/Todo.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Todo"
+};
+</script>
